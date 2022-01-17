132ce01d5672279db1727e2afa0c3f0266f1423d
Margit Tennosaar <margit.tennosaar@bc.fi>
2022-01-17 20:01:03 +0200

    first working solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Spinner from "react-bootstrap/Spinner";
 
-function App() {
+import axios from "axios";
+
+const App = () => {
+  const [pokemons, setPokemons] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon/").then((res) => {
+      const fetches = res.data.results.map((p) =>
+        axios.get(p.url).then((res) => res.data)
+      );
+
+      Promise.all(fetches).then((data) => {
+        setPokemons(data);
+        setIsLoading(false);
+      });
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#">PokeApp</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container>
+        <Row
+          xs={2}
+          md={4}
+          lg={5}
+          className="justify-content-between my-5 d-flex gap-3"
         >
-          Learn React
-        </a>
-      </header>
+          {isLoading && (
+            <Spinner animation="border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          )}
+          {!isLoading &&
+            pokemons.map((pokemon) => (
+              <Card bg="dark" text="light" key={pokemon.name}>
+                <Card.Header>{pokemon.name}</Card.Header>
+
+                <Card.Body>
+                  <Card.Img
+                    variant="top"
+                    src={pokemon.sprites.other.dream_world.front_default}
+                  />
+                </Card.Body>
+              </Card>
+            ))}
+        </Row>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
