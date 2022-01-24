7561f0a87db71e772351b9ad95a87ed6c46069b9
Margit Tennosaar <46651865+margittennosaar@users.noreply.github.com>
2022-01-24 12:59:28 +0200

    added routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
-import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PokeList from "./components/PokeList";
+
+import Layout from "./components/Layout";
+import Home from "./components/Home";
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <PokeList />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="pokelist" element={<PokeList />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
